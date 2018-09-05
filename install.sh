a563cb5a089f432e06703484940023c9657c7df9
root <root@localhost.localdomain>
2018-09-05 18:19:18 +0800

    提交修改

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 umask 022
-which ruby >/dev/null || eval "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install-ruby`"
-exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install`"
+which ruby >/dev/null || eval "`curl -fsSL https://raw.githubusercontent.com/mzenm/install/master/install-ruby`"
+exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/mzenm/install/master/install`"
